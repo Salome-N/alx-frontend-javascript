690abb9685b5e48410d2c7927246a21bde8cc65b
Salome-N <sallynjenga8@gamil.com>
2024-05-29 12:40:43 -0700

    Fix

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,4 +1,4 @@
-export default function function handleResponseFromAPI(promise) {
+export default function handleResponseFromAPI(promise) {
   return promise
     .then(() => ({ status: 200, body: 'success' }))
     .catch(() => new Error())
