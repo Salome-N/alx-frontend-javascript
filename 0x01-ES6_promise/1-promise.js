8874f1511188012b3fd5c320d4ae23d68148fb65
Salome-N <sallynjenga8@gamil.com>
2024-05-29 12:19:31 -0700

    Fix

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,13 +1,9 @@
-// eslint-disable
 export default function getFullResponseFromAPI(success) {
   return new Promise((resolve, reject) => {
-    if (success === true) {
-      resolve({
-        status: 200,
-	body: "Success"
-      });
+    if (success) {
+      resolve({ status: 200, body: 'Success'});
     } else {
-        reject(Error("The fake API is not working currently"));
+      reject(Error("The fake API is not working currently"));
     }
   });
 }
--- 0x01-ES6_promise/4-user-promise.js
+++ 0x01-ES6_promise/4-user-promise.js
@@ -1,5 +1,3 @@
 export default function signUpUser(firstName, lastName) {
-  return new promise((resolve) => {
-    resolve({ firstName, lastName });
-  });
+  return new promise.resolve({ firstName, lastName });
 }
