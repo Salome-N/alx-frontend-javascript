e9d8a6f798a0967d4b0bc11e923445c02a943136
Salome-N <sallynjenga8@gamil.com>
2024-06-04 09:03:18 -0700

    Combine

---
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,11 @@
+export default function updateStudentGradeByCity(list, city, newGrades) {
+  const studentsByCity = list.filter((student) => student.location === city);
+
+  const upStudents = studentsByCity.map((student) => {
+    const gradeObj = newGrades.find((grade) => grade.studentId === student.id);
+
+    const upGrade = gradeObj ? gradeObj.grade : 'N/A';
+    return { ...student, grade: upGrade,};
+  });
+  return upStudents;
+}
